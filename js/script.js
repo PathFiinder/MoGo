923611c14aad57c09db4ae367417390f37d90ff0
Krystian Konecki <krystian_konecki@wp.pl>
2019-09-11 10:51:33 +0200

    Adding team section

--- js/script.js
+++ js/script.js
@@ -6,14 +6,35 @@ window.addEventListener('DOMContentLoaded', function () {
 
     servicesTwoAccordion.addEventListener('click', (event) => {
         if (event.target.dataset.h) {
-
+            const targets = ["One", "Two", "Three"];
             if (document.querySelector(`#collapse${event.target.dataset.h}`).classList.contains('show')) {
+
+                targets.forEach(element => {
+                    if (element != event.target.dataset.h) {
+                        if (document.querySelector(`[data-arrow="${element}"]`).classList.contains('fa-arrow-up')) {
+                            document.querySelector(`[data-arrow="${element}"]`).classList.remove('fa-arrow-up');
+                            document.querySelector(`[data-arrow="${element}"]`).classList.add('fa-arrow-down')
+                        }
+                    }
+                })
+
                 if (document.querySelector(`[data-arrow="${event.target.dataset.h}"]`).classList.contains('fa-arrow-up')) {
                     document.querySelector(`[data-arrow="${event.target.dataset.h}"]`).classList.remove('fa-arrow-up');
                     document.querySelector(`[data-arrow="${event.target.dataset.h}"]`).classList.add('fa-arrow-down');
                 }
+
             }
             if (!document.querySelector(`#collapse${event.target.dataset.h}`).classList.contains('show')) {
+
+                targets.forEach(element => {
+                    if (element != event.target.dataset.h) {
+                        if (document.querySelector(`[data-arrow="${element}"]`).classList.contains('fa-arrow-up')) {
+                            document.querySelector(`[data-arrow="${element}"]`).classList.remove('fa-arrow-up');
+                            document.querySelector(`[data-arrow="${element}"]`).classList.add('fa-arrow-down')
+                        }
+                    }
+                })
+
                 if (document.querySelector(`[data-arrow="${event.target.dataset.h}"]`).classList.contains('fa-arrow-down')) {
                     document.querySelector(`[data-arrow="${event.target.dataset.h}"]`).classList.remove('fa-arrow-down');
                     document.querySelector(`[data-arrow="${event.target.dataset.h}"]`).classList.add('fa-arrow-up');
